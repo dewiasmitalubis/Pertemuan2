bc027c696c7fbac5691760655bdc655f792cfc52
Dewi Asmita <mytaloebis@gmail.com>
2022-03-18 14:18:35 +0700

    Percobaan When

---
+++ src/main/kotlin/16_When.kt
@@ -0,0 +1,19 @@
+fun main() {
+    //When adalah operator pengganti switch-case di bahasa pemrograman C
+    var x:Int = 5
+    when (x) {
+        1 -> println("x == 1")
+        2 -> println("x == 2")
+        else -> { // Note the block
+            println("x is neither 1 nor 2")
+        }
+    }
+    x = 2
+    when (x) {
+        1,2 -> println("Value of X either 1,2")
+        else -> { // Note the block
+            println("x is neither 1 nor 2")
+        }
+    }
+
+}
